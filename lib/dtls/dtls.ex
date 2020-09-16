031132cf9098233bce88863115cf9563aa79ab48
Michał Śledź <michal.sledz@swmansion.com>
2020-09-16 16:49:26 +0200

    Add basic API

--- lib/dtls/dtls.ex
+++ lib/dtls/dtls.ex
@@ -1,3 +1,20 @@
 defmodule DTLS do
+  require Unifex.CNode
+
+  def init(socket_path) do
+    {:ok, pid} = Unifex.CNode.start_link(:native)
+    Unifex.CNode.call(pid, :init, [socket_path])
+  end
+
+  def do_handshake() do
+    Unifex.CNode.call(pid, :do_handshake)
+    receive do
+      msg ->
+        IO.inspect(msg)
+        msg
+    after 5000 ->
+      {:timeout}
+    end
+  end
 
 end
---
+++ test/integration_test.exs
@@ -0,0 +1,5 @@
+defmodule DTLS.Test do
+  use ExUnit.Case, async: true
+
+  test ""
+end
